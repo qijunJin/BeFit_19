24a20205474c174117da7b26718aee562a1abe1e
albertPerezUb <albertperezcosta99@gmail.com>
2020-03-16 19:32:28 +0100

    #1 Vista layout Welcome, fet

--- app/src/main/java/com/example/befit/MainActivity.kt
+++ app/src/main/java/com/example/befit/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.befit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn_signUp : Button = findViewById(R.id.btn_signUp)
+        var btn_signIn : Button = findViewById(R.id.btn_SignIn)
+
     }
 }
