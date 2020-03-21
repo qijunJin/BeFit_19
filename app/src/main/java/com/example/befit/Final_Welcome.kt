6e03401ef7659be850780bca80e452b33c993190
albertPerezub <albertperezcosta99@gmail.com>
2020-03-21 15:19:19 +0100

    #1 Falta fer fragmentThree y retocar algunas coses

--- app/src/main/java/com/example/befit/Final_Welcome.kt
+++ app/src/main/java/com/example/befit/Final_Welcome.kt
@@ -2,14 +2,17 @@ package com.example.befit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_one.*
 
 class Final_Welcome : AppCompatActivity() {
-
+lateinit var welcome_name : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final__welcome)
         Toast.makeText(this, "User created", Toast.LENGTH_SHORT).show()
+
 
     }
 }
--- app/src/main/java/com/example/befit/FragmentTwo.kt
+++ app/src/main/java/com/example/befit/FragmentTwo.kt
@@ -1,10 +1,17 @@
 package com.example.befit
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_two.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +23,40 @@ class FragmentTwo : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false)
+
+        //Interacció amb els botons del fragment
+        var mLinearLayout = inflater.inflate(R.layout.fragment_two,container,false)
+        var btn_ubication : Button = mLinearLayout.findViewById(R.id.btn_ubication)
+        var btn_notif : Button = mLinearLayout.findViewById(R.id.btn_notification)
+        var btn_monday : RadioButton = mLinearLayout.findViewById(R.id.btn_Monday)
+
+
+        btn_ubication.setOnClickListener {
+            if(btn_ubication.text=="Allow"){
+                btn_ubication.text = "Allowed"
+                btn_ubication.setBackgroundColor(Color.parseColor("#F6DE05"))
+            }else if(btn_ubication.text=="Allowed"){
+                btn_ubication.text="Allow"
+                btn_ubication.setBackgroundColor(Color.parseColor("#595656"))
+            }
+        }
+
+        btn_notif.setOnClickListener {
+            if(btn_notif.text=="Allow"){
+                btn_notif.text = "Allowed"
+                btn_notif.setBackgroundColor(Color.parseColor("#F6DE05"))
+            }else if(btn_notif.text=="Allowed"){
+                btn_notif.text="Allow"
+                btn_notif.setBackgroundColor(Color.parseColor("#595656"))
+            }
+        }
+
+        return mLinearLayout  //Retornem el layout despres dels canvis
+        //return inflater.inflate(R.layout.fragment_two, container, false)
     }
+
+
+
+
 
 }
