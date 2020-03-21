5a3fc37ccb965510ca4ceb9f7a8b0bf9d698184b
qijunJin <qijunjin24@gmail.com>
2020-03-21 15:44:39 +0100

    merge 1

--- app/src/main/java/com/example/befit/FragmentOne.kt
+++ app/src/main/java/com/example/befit/FragmentOne.kt
@@ -1,13 +1,10 @@
 package com.example.befit
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_final__welcome.*
-import kotlinx.android.synthetic.main.fragment_one.*
+import androidx.fragment.app.Fragment
 
 /**
  * A simple [Fragment] subclass.
