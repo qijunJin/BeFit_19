b1d82fdad64958e09c186774f393665f4215b8eb
ojimenmu <ojimenmu@gmail.com>
2020-06-07 16:05:12 +0200

    Creadas typo de comida y espacio para recomendados en el fragment

--- app/src/main/java/com/example/befit/fragment_comida.kt
+++ app/src/main/java/com/example/befit/fragment_comida.kt
@@ -13,6 +13,11 @@ import android.widget.ListView
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
+var foodList: ArrayList<Comida> = ArrayList()
+
+var FoodPreferences : ArrayList<FoodType> = ArrayList()
+
 /**
  * A simple [Fragment] subclass.
  * Use the [fragment_comida.newInstance] factory method to
@@ -29,9 +34,50 @@ class fragment_comida : Fragment() {
         // Inflate the layout for this fragment
         var mLinearLayout = inflater.inflate(R.layout.fragment_comida, container, false)
         var comidaListView: ListView = mLinearLayout.findViewById(R.id.listComida)
+        var preferenceListView: ListView = mLinearLayout.findViewById(R.id.listOfFavorites)
         var comidaList = GetArrayItems()
+        var preferenceList = GetArrayFavorites()
         var comidaAdapter = ComidaAdapter(this.context, comidaList)
+        var preferenceAdapter = ComidaAdapter(this.context, preferenceList)
         comidaListView.adapter = comidaAdapter
+        preferenceListView.adapter = preferenceAdapter
+
+        //Food initzialization
+        foodList.add(Comida(R.drawable.seafood_paella, "Seafood Paella", 379,
+            ingredients = mutableListOf("Rice","Fish Stock","Musseles", "Shrimp"), description = "This is the description",
+        type= FoodType.MEDITERRANEAN))
+        foodList.add(Comida(R.drawable.baked_italian_chicken, "Baked Chicken", 340,
+            ingredients = mutableListOf("Chicken","Potatoes","Cherry Tomatoes", "Olive oil"), description = "This is the description",
+            type= FoodType.MEDITERRANEAN))
+        foodList.add(Comida(R.drawable.spring_rolls, "Spring Rolls", 154,
+            ingredients = mutableListOf("Grated Vegetables","Flour","Salt and pepper", "Soy Sauce"), description = "This is the description",
+            type= FoodType.ORIENTAL))
+        foodList.add(Comida(R.drawable.ramen_adobe_m, "Ramen", 200,
+            ingredients = mutableListOf("Sliced Cooked Pork","Garlic Coves","Ramen noodles", "Sesame Oil"), description = "This is the description",
+            type= FoodType.ORIENTAL))
+        foodList.add(Comida(R.drawable.cobb_salad, "Cobb Salad", 400,
+            ingredients = mutableListOf("Avocado","Tomato", "Hard Boiled Egg","Dressing"), description = "This is the description",
+            type= FoodType.AMERICAN))
+        foodList.add(Comida(R.drawable.fried_chicken, "Fried Chicken", 190,
+            ingredients = mutableListOf("Chicken","Flour","Oil"), description = "This is the description",
+            type= FoodType.AMERICAN))
+        foodList.add(Comida(R.drawable.kibbeh, "Kibbeh", 365,
+            ingredients = mutableListOf("Fine Bulgur Wheat","Cinnamon","Beef", "Salt and Pepper"), description = "This is the description",
+            type= FoodType.HALAL))
+        foodList.add(Comida(R.drawable.nachos, "Nachos", 346,
+            ingredients = mutableListOf("Refried Beans","Cheese","Guacamole", "Jalapeño"), description = "This is the description",
+            type= FoodType.MEXICAN))
+        foodList.add(Comida(R.drawable.tamales, "Chicken Tamales", 141,
+            ingredients = mutableListOf("Pork Loin","Flour Dough","Dried Corn Husks", "Sour Cream"), description = "This is the description",
+            type= FoodType.MEXICAN))
+        foodList.add(Comida(R.drawable.pasta_salad, "Pasta Salad", 356,
+            ingredients = mutableListOf("Cooked Pasta","Cherry Tomatoes","Summer Sausage", "Red Onion"), description = "This is the description",
+            type= FoodType.VEGETARIAN))
+        foodList.add(Comida(R.drawable.mushroom_burger, "Mushroom Burger", 850,
+            ingredients = mutableListOf("Diced Mushrooms","Onion","Egg Replacer", "Pinto Beans"), description = "This is the description",
+            type= FoodType.VEGETARIAN))
+
+        FoodPreferences.add(FoodType.MEDITERRANEAN) //TODO: CHANGE
 
         comidaListView.onItemClickListener =
             AdapterView.OnItemClickListener { _: AdapterView<*>, _: View, i: Int, _: Long ->
@@ -40,34 +86,35 @@ class fragment_comida : Fragment() {
                 startActivity(intent)
             }
 
+        preferenceListView.onItemClickListener =
+            AdapterView.OnItemClickListener { _: AdapterView<*>, _: View, i: Int, _: Long ->
+                var intent = Intent(this.context, GoToEat::class.java)
+                intent.putExtra("comida",GetArrayFavorites().get(i))
+                startActivity(intent)
+            }
+
         return mLinearLayout
     }
 
     private fun GetArrayItems(): ArrayList<Comida> {
-        var listItems = ArrayList<Comida>()
-        listItems.add(Comida(R.drawable.seafood_paella, "Seafood Paella", 379,
-            ingredients = mutableListOf("Rice","Fish Stock","Musseles", "Shrimp"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.baked_italian_chicken, "Baked Chicken", 340,
-            ingredients = mutableListOf("Chicken","Potatoes","Cherry Tomatoes", "Olive oil"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.spring_rolls, "Spring Rolls", 154,
-            ingredients = mutableListOf("Grated Vegetables","Flour","Salt and pepper", "Soy Sauce"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.ramen_adobe_m, "Ramen", 200,
-            ingredients = mutableListOf("Sliced Cooked Pork","Garlic Coves","Ramen noodles", "Sesame Oil"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.cobb_salad, "Cobb Salad", 400,
-            ingredients = mutableListOf("Avocado","Tomato", "Hard Boiled Egg","Dressing"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.fried_chicken, "Fried Chicken", 190,
-            ingredients = mutableListOf("Chicken","Flour","Oil"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.kibbeh, "Kibbeh", 365,
-            ingredients = mutableListOf("Fine Bulgur Wheat","Cinnamon","Beef", "Salt and Pepper"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.nachos, "Nachos", 346,
-            ingredients = mutableListOf("Refried Beans","Cheese","Guacamole", "Jalapeño"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.tamales, "Chicken Tamales", 141,
-            ingredients = mutableListOf("Pork Loin","Flour Dough","Dried Corn Husks", "Sour Cream"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.pasta_salad, "Pasta Salad", 356,
-            ingredients = mutableListOf("Cooked Pasta","Cherry Tomatoes","Summer Sausage", "Red Onion"), description = "This is the description"))
-        listItems.add(Comida(R.drawable.mushroom_burger, "Mushroom Burger", 850,
-            ingredients = mutableListOf("Diced Mushrooms","Onion","Egg Replacer", "Pinto Beans"), description = "This is the description"))
-        return listItems
+
+        var list : ArrayList<Comida> = ArrayList<Comida>()
+        for (food in foodList){
+            if (food.type !in FoodPreferences){
+                list.add(food)
+            }
+        }
+        return list
+    }
+
+    private fun GetArrayFavorites(): ArrayList<Comida> {
+        var list = ArrayList<Comida>()
+        for (food in foodList){
+            if (food.type in FoodPreferences){
+                list.add(food)
+            }
+        }
+        return list
     }
 
     companion object {
